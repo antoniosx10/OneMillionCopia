bbbf2581606d9047e2a46f18979a5b6018a11f11
antoniosx10 <32486516+antoniosx10@users.noreply.github.com>
2018-04-01 01:39:27 +0200

    Fix

--- app/src/main/java/com/example/antonio/todash_copia/MainActivity.java
+++ app/src/main/java/com/example/antonio/todash_copia/MainActivity.java
@@ -5,11 +5,15 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthProvider;
@@ -24,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private TextView prova;
-
+    private FirebaseAuth mAuth;
     private static final int RC_SIGN_IN = 123;
 
     @Override
@@ -32,35 +36,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build());
+        mAuth =  FirebaseAuth.getInstance();
 
-
-// Create and launch sign-in intent
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
-                        .build(),
-                RC_SIGN_IN);
-
-
-
-
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
-            //If the draw over permission is not available open the settings screen
-            //to grant the permission.
-            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:" + getPackageName()));
-            startActivityForResult(intent, 1000);
-        }
-
-        //Usato per testare senza dover ogni volta spegnere e accendere il dispositivo.
-        Intent serviceIntent = new Intent(getApplicationContext(), ListenerService.class);
-        startService(serviceIntent);
-
-        prova = findViewById(R.id.provaTesto);
     }
 
     @Override
@@ -73,12 +50,53 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+                    //If the draw over permission is not available open the settings screen
+                    //to grant the permission.
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                            Uri.parse("package:" + getPackageName()));
+                    startActivityForResult(intent, 1000);
+                }
+
+                //Usato per testare senza dover ogni volta spegnere e accendere il dispositivo.
+                Intent serviceIntent = new Intent(getApplicationContext(), ListenerService.class);
+                startService(serviceIntent);
+
+                prova = findViewById(R.id.provaTesto);
                 // ...
             } else {
                 // Sign in failed, check response for error code
                 // ...
             }
         }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null) {
+
+        } else {
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                    new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build());
+
+
+// Create and launch sign-in intent
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    RC_SIGN_IN);
+        }
+    }
+
+    public void sign_out(View v){
+        mAuth.signOut();
     }
 
 }
